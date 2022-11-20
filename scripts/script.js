12a3577f82f0095337d33372a18f420c1770f62a
Nick <ZippyIO@users.noreply.github.com>
2022-11-20 17:23:32 +1100

    page updates with calculated distance after submit button

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 const form = document.querySelector('form');
+const distancePara = document.querySelector('.calc-distance');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    distancePara.textContent = 'Distance: ' + calculateDistance() + 'km away';
     console.log(calculateDistance());
 });
 
